b2b34bf9118aecf1296e5ffe611ade2698d5741e
박태준 <ptj01036251@gmail.com>
2024-08-12 21:37:29 +0900

    테마 및 screenutil 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'LoginPage/login_screen.dart';
 
 void main(){
@@ -10,16 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        bottomSheetTheme: const BottomSheetThemeData(
-          backgroundColor: Colors.white,
+    return ScreenUtilInit(
+      designSize: const Size(393, 853),
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.white
+          ),
+          bottomSheetTheme: const BottomSheetThemeData(
+            backgroundColor: Colors.white,
+          ),
+          fontFamily: "Pretendard",
         ),
-        fontFamily: "Pretendard",
+        debugShowCheckedModeBanner: false,
+        home: const LoginScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
     );
   }
 }
